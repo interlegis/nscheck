5a87074356558f039bb1ed653080056285f1c258
Vitor Choi Feitosa <vchoi@vchoi.org>
2016-09-14 18:06:27 -0300

    Changed to check for a single domain.
    . Default timeout changed to 2.9s (zabbix default timeout is 3s)

--- nscheck.py
+++ nscheck.py
@@ -15,21 +15,16 @@ def ip_or_hostname(argument):
 # Parse command line args
 parser = argparse.ArgumentParser(description='Test leg.br nameserver.')
 parser.add_argument("NAMESERVER", type=ip_or_hostname,
-        help = 'IP address or hostname of server to test.')
+        help = 'IP address or hostname of nameserver to test.')
+parser.add_argument("DOMAIN", type=str,
+        help = 'Domain to ask SOA record for.')
 parser.add_argument('--tcp', action='store_true',
         help = 'Use TCP instead of UDP')
-parser.add_argument('--timeout', action='store', type=float, default=5.0,
-        help = 'Timeout for DNS queries in seconds. Default to 5.0s')
+parser.add_argument('--timeout', action='store', type=float, default=2.9,
+        help = 'Timeout for DNS queries in seconds. Default to 2.9s')
 parser.add_argument('--verbose', action='store_true',
         help = 'Verbose output.')
 args = parser.parse_args()
-
-UFS=['ac', 'al', 'ap', 'am', 'ba', 'ce', 'df', 'es', 'go', 'ma', 'mt',
-        'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs',
-        'ro', 'rr', 'sc', 'sp', 'se', 'to']
-DOMAINS=['infra.leg.br']
-for uf in UFS:
-    DOMAINS.append(uf+'.leg.br')
 
 # dns resolver setup
 resolver = dns.resolver.Resolver()
@@ -42,27 +37,14 @@ def logi(message):
     if args.verbose:
         print(message)
 
-logi('Testing %i domains: ' % len(DOMAINS))
-failed_domains=[]
-ok_domains=[]
+logi('Testing domain %s on nameserver %s' % (args.DOMAIN, args.NAMESERVER))
 start_time = time.time()
-for domain in DOMAINS:
-    try:
-        answer = resolver.query(domain, 'SOA', tcp=args.tcp)
-    except Exception as e:
-        print('E', end='', flush=True)
-        failed_domains.append((domain, e))
-        continue
-    print('.', end='', flush=True)
-    soa=answer[0]
-    ok_domains.append((domain, soa))
+try:
+    answer = resolver.query(args.DOMAIN, 'SOA', tcp=args.tcp)
+    print("OK %s, IN SOA: %s" % (args.DOMAIN, answer[0]))
+except Exception as e:
+    print("ERR %s" % args.DOMAIN, type(e), str(e), sep=', ')
 end_time = time.time()
-print('')
 
-# test results
-for domain, soa in ok_domains:
-    logi("OK %s, %s" % (domain, soa))
-for domain, exception in failed_domains:
-    print("ERR", domain, type(exception), str(exception), sep=', ')
 logi("Elapsed time: %0.3f seconds" % (end_time - start_time))
 
